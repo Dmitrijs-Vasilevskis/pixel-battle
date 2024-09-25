5e3dfd2a59c659f76fd4e1501254ec304de6534c
Dmitrijs-Vasilevskis <dmitrij.vasilevski@gmail.com>
2024-09-25 17:52:20 +0300

    add global styles, theme, default pallete colors

---
+++ pixel-battle-app/src/app/context/defaulPallete.js
@@ -0,0 +1,14 @@
+export const defaultPalette = [
+  { name: "Red", hex: "#FF0000" },
+  { name: "Blue", hex: "#0000FF" },
+  { name: "Green", hex: "#00FF00" },
+  { name: "Yellow", hex: "#FFFF00" },
+  { name: "Orange", hex: "#FFA500" },
+  { name: "Purple", hex: "#800080" },
+  { name: "Pink", hex: "#FFC0CB" },
+  { name: "Brown", hex: "#A52A2A" },
+  { name: "Black", hex: "#000000" },
+  { name: "White", hex: "#FFFFFF" },
+  { name: "Cyan", hex: "#00FFFF" },
+  { name: "Magenta", hex: "#FF00FF" },
+];
---
+++ pixel-battle-app/src/app/utils/icons.js
@@ -0,0 +1,4 @@
+export const paletteIcon = <i className="fa-solid fa-palette"></i>;
+export const xmark = <i className="fa-solid fa-xmark"></i>;
+export const login = <i className="fa-solid fa-right-to-bracket"></i>;
+export const logout = <i className="fa-solid fa-right-from-bracket"></i>;
